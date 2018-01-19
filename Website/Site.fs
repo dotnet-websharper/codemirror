5f3d8cc9eb4c6e113f58df92d608b8c28a005b90
Loic Denuziere <loic@denuziere.net>
2018-01-19 17:24:36 +0100

    Convert to .NET Standard

--- Website/Site.fs
+++ Website/Site.fs
@@ -2,42 +2,39 @@
 
 open WebSharper
 
-type Action = Index
-
+[<JavaScript>]
 module Client =
 
     open WebSharper.JavaScript
-    open WebSharper.Html.Client
+    open WebSharper.UI
+    open WebSharper.UI.Client
+    open WebSharper.UI.Html
     open WebSharper.CodeMirror
 
     [<Require(typeof<CodeMirror.Resources.Modes.Javascript>)>]
     [<Require(typeof<CodeMirror.Resources.Modes.Xml>)>]
     [<Require(typeof<CodeMirror.Resources.Modes.Htmlmixed>)>]
-    [<Sealed>]
-    type Control() =
-        inherit Web.Control()
-
-        [<JavaScript>]
-        override this.Body =
-            let options =
-                CodeMirror.Options(
-                    Mode = "javascript",
-                    LineNumbers = true,
-                    ExtraKeys = New [
-                        "Ctrl-Space", box(fun cm ->
-                            CodeMirror.ShowHint(cm, Hint.JavaScript()))
-                    ]
-                )
-            let cm =
-                CodeMirror.FromTextArea(
-                    JS.Document.GetElementById "editor",
-                    options)
-            // cm.OnCursorActivity(fun cm ->
-            //             cm.MatchHighlight(MatchHighlighter("match-highlight")))
-            //cm.OnGutterClick(CodeMirror.NewFoldFunction(RangeFinder(CodeMirror.IndentRangeFinder)))
-            Console.Log (cm.CharCoords(CharCoords(1, 1), CoordsMode.Page))
-            let dial = cm.OpenDialog(Dialog("bar:<input/>"), fun x -> Console.Log(x))
-            let resultContainer = Pre [Attr.Class "cm-s-default"]
+    [<SPAEntryPoint>]
+    let Main() =
+        let options =
+            CodeMirror.Options(
+                Mode = "javascript",
+                LineNumbers = true,
+                ExtraKeys = New [
+                    "Ctrl-Space", box(fun cm ->
+                        CodeMirror.ShowHint(cm, Hint.JavaScript()))
+                ]
+            )
+        let cm =
+            CodeMirror.FromTextArea(
+                JS.Document.GetElementById "editor",
+                options)
+        // cm.OnCursorActivity(fun cm ->
+        //             cm.MatchHighlight(MatchHighlighter("match-highlight")))
+        //cm.OnGutterClick(CodeMirror.NewFoldFunction(RangeFinder(CodeMirror.IndentRangeFinder)))
+        Console.Log (cm.CharCoords(CharCoords(1, 1), CoordsMode.Page))
+        let dial = cm.OpenDialog(Dialog("bar:<input/>"), fun x -> Console.Log(x))
+        let resultContainer = pre [Attr.Class "cm-s-default"] []
 
 //            let cmXml =
 //                let options =
@@ -51,45 +48,13 @@ module Client =
 //                CodeMirror.FromTextArea(
 //                    JS.Document.GetElementById "editor-xml", options)
 
-            Div [
-                Button [Text "Run mode"]
-                |>! OnClick (fun _ _ ->
+        div [] [
+            button [
+                on.click (fun _ _ ->
                     CodeMirror.RunMode(cm.GetValue(), "javascript",
                         RunModeOutput(resultContainer.Dom)))
-                Button [Text "Clear"]
-                |>! OnClick (fun _ _ -> resultContainer.Clear())
-                resultContainer
-            ] :> _
-
-open WebSharper.Sitelets
-
-module Site =
-
-    open WebSharper.Html.Server
-
-    type Page =
-        {
-            Title : string
-            Body : list<Element>
-        }
-
-    let Template =
-        Content.Template<Page>(System.Web.HttpContext.Current.Server.MapPath "/Main.html")
-            .With("title", fun p -> p.Title)
-            .With("body", fun p -> p.Body)
-
-    let Index ctx =
-        Content.WithTemplate Template
-            {
-                Title = "CodeMirror sample"
-                Body = [Div [new Client.Control()]]
-            }
-
-/// The class that contains the website
-type Website() =
-    interface IWebsite<Action> with
-        member this.Sitelet = Sitelet.Content "/" Index Site.Index
-        member this.Actions = [Index]
-
-[<assembly: WebsiteAttribute(typeof<Website>)>]
-do ()
+            ] [text "Run mode"]
+            button [on.click (fun _ _ -> resultContainer.Clear())] [text "Clear"]
+            resultContainer
+        ]
+        |> Doc.RunAppend JS.Document.Body
--- build.fsx
+++ build.fsx
@@ -1,95 +1,17 @@
-#load "tools/includes.fsx"
-#r "System.IO.Compression.FileSystem"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.CodeMirror")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-open System.IO
-let ( +/ ) a b = Path.Combine(a, b)
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-open System.Text.RegularExpressions
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-let getResources() =
-    let ( @- ) a b = Seq.append a b
-
-    let tempdir = __SOURCE_DIRECTORY__ +/ ".temp"
-    Directory.CreateDirectory tempdir |> ignore
-
-    use cl = new System.Net.WebClient()
-
-    let version = 
-        Regex(">version ((?:[0-9]|\.)*)<")
-            .Match(cl.DownloadString "http://codemirror.net/doc/manual.html")
-            .Groups.[1].Value
-
-    printfn "Codemirror version: %s" version
-
-    let zipfile =  tempdir +/ "codemirror.zip"
-    let unzipped = tempdir +/ ("codemirror-" + version)
-
-    if not <| Directory.Exists unzipped then
-        printfn "donwloading http://codemirror.net/codemirror.zip"
-        cl.DownloadFile("http://codemirror.net/codemirror.zip", zipfile)
-        Compression.ZipFile.ExtractToDirectory(zipfile, unzipped)
-
-    let inner = Directory.GetDirectories(unzipped).[0]
-
-    let res =
-        [|
-            for d in [| "addon"; "keymap"; "lib"; "mode"; "theme" |] do
-                let d = inner +/ d
-                for p in Directory.GetFiles(d, "*.js", SearchOption.AllDirectories) do
-                    if Path.GetFileName p <> "test.js" then
-                        File.WriteAllText(p,
-                            "if(typeof(define) == 'function'){__define__old=define;delete define}\n"
-                            + File.ReadAllText(p)
-                            + "\nif(typeof(__define__old) == 'function'){define=__define__old;delete __define__old}")
-                        yield p
-                yield! Directory.GetFiles(d, "*.css", SearchOption.AllDirectories)
-        |]
-
-    File.WriteAllLines(tempdir +/ "res.txt", res |> Seq.map (fun r -> r.[inner.Length + 1 ..]))
-
-    res
-
-let main =
-    bt.WebSharper.Extension("WebSharper.CodeMirror")
-        .SourcesFromProject()
-        .Embed(getResources())
-
-let website =
-    bt.WebSharper.Library("Website")
-        .SourcesFromProject()
-        .References(fun r ->
-            [
-                r.Assembly("System.Web")
-                r.NuGet("WebSharper.Html").Version("(,4.0)").ForceFoundVersion().Reference()
-                r.Project main
-            ])
-
-let web =
-    bt.WebSharper.HostWebsite("Web")
-        .References(fun r ->
-            [
-                r.Project main
-                r.Project website
-                r.NuGet("WebSharper").Version("(,4.0)").At(["/tools/net45/IntelliFactory.Xml.dll"]).Reference()
-                r.NuGet("WebSharper.Html").Version("(,4.0)").ForceFoundVersion().Reference()
-            ])
-
-bt.Solution [
-
-    main
-    website
-    web
-
-    bt.NuGet.CreatePackage()
-        .Add(main)
-        .Description("CodeMirror bindings for WebSharper")
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
