eeb8cf9fa372c15dce4ea0221ed64731c7f7d45a
András Jankó <andras.janko@gmail.com>
2021-01-03 15:38:30 +0100

    Update to dotnet Fake/Paket tooling

--- build.fsx
+++ build.fsx
@@ -1,6 +1,31 @@
+﻿#if INTERACTIVE
+#r "nuget: FAKE.Core"
+#r "nuget: Fake.Core.Target"
+#r "nuget: Fake.IO.FileSystem"
+#r "nuget: Fake.Tools.Git"
+#r "nuget: Fake.DotNet.Cli"
+#r "nuget: Fake.DotNet.AssemblyInfoFile"
+#r "nuget: Fake.DotNet.Paket"
+#r "nuget: Paket.Core"
+#else
+#r "paket:
+nuget FAKE.Core
+nuget Fake.Core.Target
+nuget Fake.IO.FileSystem
+nuget Fake.Tools.Git
+nuget Fake.DotNet.Cli
+nuget Fake.DotNet.AssemblyInfoFile
+nuget Fake.DotNet.Paket
+nuget Paket.Core //"
+#endif
+
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
 #r "System.IO.Compression.FileSystem"
-open Fake
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
 open WebSharper.Fake
 open System.IO
 open System.Text.RegularExpressions
@@ -11,7 +36,7 @@ let targets =
     WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion)
     |> MakeTargets
 
-Target "Get-Resources" <| fun () ->
+Target.create "Get-Resources" <| fun _ ->
     let ( @- ) a b = Seq.append a b
 
     let tempdir = __SOURCE_DIRECTORY__ +/ ".temp"
@@ -62,10 +87,6 @@ Target "Get-Resources" <| fun () ->
 
 targets.AddPrebuild "Get-Resources"
 
-Target "Build" DoNothing
-targets.BuildDebug ==> "Build"
+targets.Publish ==> "CI-Release"
 
-Target "CI-Release" DoNothing
-targets.CommitPublish ==> "CI-Release"
-
-RunTargetOrDefault "Build"
+Target.runOrDefault "Build"
